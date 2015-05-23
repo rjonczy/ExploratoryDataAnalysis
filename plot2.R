e8617828e1628163ba7818ec84e6ea5960e03a5c
Robert Jonczy <robert.jonczy@omicrons.pl>
2015-05-23 17:22:15 +0200

    plot2.R implemented

--- plot2.R
+++ plot2.R
@@ -1,22 +1,24 @@
 # load data from rds files
 NEI <- readRDS("summarySCC_PM25.rds")
 
+# subset only Baltimore (fips = '24510')
+NEI.baltimore <- NEI[NEI$fips == '24510',]
+
 # sum all Emissions and aggregate by year
-totals <- aggregate(Emissions ~ year, NEI, FUN = sum)
+totals <- aggregate(Emissions ~ year, NEI.baltimore, FUN = sum)
 
 # setup file to be used for plotting
-png(filename = "plot1.png")
+png(filename = "plot2.png")
 
 # use barplot to plot total emission per year for all sources
 barplot(
-    totals$Emissions / 1000000,
+    totals$Emissions,
     names.arg = totals$year,
     col = c('red', 'green', 'blue', 'yellow'),
     xlab = "Year",
-    ylab = "Total Emmission in [mln of Tones]",
-    main = "PM2.5 emission between 1999-2008 (all US)"
+    ylab = "Total Emmission in [Tones]",
+    main = "PM2.5 emission between 1999-2008 (Baltimore)"
 )
 
 dev.off()
-
 
