7055318b1746ebc91bc75f170bf8a98b6087be2d
Robert Jonczy <robert.jonczy@omicrons.pl>
2015-05-23 17:05:28 +0200

    added ploting to png file

--- plot1.R
+++ plot1.R
@@ -4,6 +4,9 @@ NEI <- readRDS("summarySCC_PM25.rds")
 # sum all Emissions and aggregate by year
 totals <- aggregate(Emissions ~ year, NEI, FUN = sum)
 
+# setup file to be used for plotting
+png(filename = "plot1.png")
+
 # use barplot to plot total emission per year for all sources
 barplot(
     totals$Emissions / 1000000,
@@ -13,5 +16,7 @@ barplot(
     ylab = "Total Emmission",
     main = "PM2.5 emission between 1999-2008"
 )
+
+dev.off()
 
 
